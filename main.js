6425d357cd155c3f1bc14962353b5417ff1a3e3d
Ezequiel Nieto <nietoez@hotmail.com>
2023-01-19 16:25:17 -0300

    ULTIMOO 19-1-23#

---
+++ main.js
@@ -0,0 +1,42 @@
+/*var icono1 = document.getElementById("icn1");
+var icono2 = document.getElementById("icn2");
+var icono3 = document.getElementById("icn3");
+*/
+var iconos = document.getElementsByClassName('iconos1');
+console.log(iconos)
+
+
+for (let i = 0; i < iconos.length; i++){
+     
+      li.iconos1.addEventListener('mouseover', cambiarColor);
+      li.iconos1.addEventListener('mouseout', volverColor);
+}
+  
+
+    
+   
+
+
+function cambiarColor(element){
+
+
+    li.iconos1.style.color = '#007B04'; 
+}
+            
+    
+ 
+                        
+
+function volverColor(){
+
+             li.iconos1.style.color = 'black'; 
+       
+                        }
+
+
+
+  
+
+       
+    
+    
